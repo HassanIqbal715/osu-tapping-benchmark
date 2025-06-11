7db2026f392b803339f65ed47926f4c7de490c5e
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2025-06-11 12:26:37 +0500

    Created clicker

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("active")
+const mainBox = document.querySelector("#main-box")
+var clicks = 0
+
+document.body.addEventListener("keypress", (e)=> {
+    if (e.key == 'z' || e.key == 'x') {
+        clicks += 1
+        mainBox.textContent = clicks
+    }
+});
