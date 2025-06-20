b72763a37542e69b0460c16e43dc04e5a2cdd24e
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2025-06-20 18:39:03 +0500

    Key must be lifted to register another input now. Reset button changed to 'r'

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
-const mainBox = document.querySelector("#total-clicks")
-var clicks = 0
+const totalClicks = document.querySelector("#total-clicks");
+const bpmElement = document.querySelector("#BPM");
+const cpsElement = document.querySelector("#CPS");
 
-document.body.addEventListener("keypress", (e)=> {
-    if (e.key == 'z' || e.key == 'x') {
-        clicks += 1
-        mainBox.textContent = clicks
+var clicks = 0;
+var BPM = 0;
+var CPS = 0.0;
+var key1Flag = false;
+var key2Flag = false;
+
+function reset() {
+    key1Flag = false;
+    key2Flag = false;
+    clicks = 0;
+    totalClicks.textContent = clicks;
+}
+
+function calculateBPM() {
+
+}
+
+document.addEventListener("keydown", (e)=> {
+    if (e.key == 'z' && !key1Flag) {
+        key1Flag = true;
+        clicks += 1;
+        totalClicks.textContent = clicks;
+    }
+    if (e.key == 'x' && !key2Flag) {
+        key2Flag = true;
+        clicks += 1;
+        totalClicks.textContent = clicks;
+    }
+    if (e.key == 'r') {
+        reset();
+    }
+});
+
+document.addEventListener("keyup", (e) => {
+    if (e.key == 'z') {
+        key1Flag = false;
+    }
+    if (e.key == 'x') {
+        key2Flag = false;
     }
 });
