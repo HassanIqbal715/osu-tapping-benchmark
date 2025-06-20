3d850b396acb7f676232175fe091198b8a997670
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2025-06-20 18:57:08 +0500

    Added a function for each key tap. Added a timer and a sample targetTime

--- script.js
+++ script.js
@@ -5,12 +5,22 @@ const cpsElement = document.querySelector("#CPS");
 var clicks = 0;
 var BPM = 0;
 var CPS = 0.0;
+
 var key1Flag = false;
 var key2Flag = false;
+var stopped = false;
+var started = false;
+
+var timeElapsed = 0;
+var targetTime = 5;
+var timer;
 
 function reset() {
     key1Flag = false;
     key2Flag = false;
+    stopped = false;
+    started = false;
+    timeElapsed = 0;
     clicks = 0;
     totalClicks.textContent = clicks;
 }
@@ -19,16 +29,35 @@ function calculateBPM() {
 
 }
 
-document.addEventListener("keydown", (e)=> {
-    if (e.key == 'z' && !key1Flag) {
-        key1Flag = true;
-        clicks += 1;
-        totalClicks.textContent = clicks;
+function countTime() {
+    if (timeElapsed < targetTime - 1) {
+        timeElapsed += 1;
+        console.log(timeElapsed);
     }
-    if (e.key == 'x' && !key2Flag) {
+    else {
+        clearInterval(timer);
+        console.log("Cleared");
+        stopped = true;
+    }
+}
+
+function keyTap() {
+    clicks += 1;
+    totalClicks.textContent = clicks;
+    if (!started) {
+        started = true;    
+        timer = setInterval(countTime, 1000);
+    }
+}
+
+document.addEventListener("keydown", (e)=> {
+    if (e.key == 'z' && !key1Flag && !stopped) {
+        key1Flag = true;
+        keyTap();
+    }
+    if (e.key == 'x' && !key2Flag && !stopped) {
         key2Flag = true;
-        clicks += 1;
-        totalClicks.textContent = clicks;
+        keyTap();
     }
     if (e.key == 'r') {
         reset();
